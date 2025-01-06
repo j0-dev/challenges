148722e6fbc15519fc96b75b60ff0114a5ccf08c
Josh Birch <2312292@chester.ac.uk>
2025-01-06 10:34:26 +0000

    Finish "Convert number to a string" Codewars Java kata

---
+++ codewars/java/numToString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class numToString 
+{
+    public static void main(String[] args) 
+    {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int userNumber = inputScanner.nextInt();
+
+        String result = numberToString(userNumber);
+        System.out.println("The number as a string is: " + result);
+    }  
+    
+    public static String numberToString(int num)
+    {
+        return String.valueOf(num); 
+    }
+}
