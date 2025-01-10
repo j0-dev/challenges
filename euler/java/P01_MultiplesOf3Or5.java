62cc8e0099bbbd6f0648ea2e91aeaaa77decd73d
Josh Birch <2312292@chester.ac.uk>
2025-01-10 12:07:00 +0000

    Organise files and complete two Euler challenges - Cleaned up file names for compatibility across all folders. - Completed problem one ("Multiples of 3 or 5") in Java. - Completed problem two ("Even Fibonacci Numbers") in Java.

---
+++ euler/java/P01_MultiplesOf3Or5.java
@@ -0,0 +1,19 @@
+package euler.java;
+
+public class P01_MultiplesOf3Or5 
+{
+    public static void main(String[] args) 
+    {
+        int sum = 0;
+
+        for(int i = 1; i < 1000; i++)
+        {
+            if(i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+        
+        System.out.println(sum);
+    }
+}
---
+++ euler/java/P02_EvenFibonacciNum.java
@@ -0,0 +1,27 @@
+package euler.java;
+
+public class P02_EvenFibonacciNum 
+{
+    public static void main(String[] args) 
+    {
+        int fib = 1;
+        int next = 2;
+        int sum = 0;
+ 
+        while(fib < 4000000)
+        {
+            if(fib % 2 == 0)
+            {
+                sum += fib;
+            }
+            
+            int temp = fib + next;
+            fib = next;
+            next = temp;
+
+        }
+
+        System.out.println(sum);
+    }       
+}    
+
