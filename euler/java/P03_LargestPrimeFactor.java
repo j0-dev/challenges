7bc76ea0256d6f5e66e441a4fc7da4a97064a970
Josh Birch <2312292@chester.ac.uk>
2025-01-11 14:15:33 +0000

    Starts work on Euler problem 3 (Largest Prime Factor) in Java

---
+++ euler/java/P03_LargestPrimeFactor.java
@@ -0,0 +1,19 @@
+package euler.java;
+
+public class P03_LargestPrimeFactor 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+    static boolean isPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return False;
+        }
+
+        for(int i = 2; )
+    }
+}
